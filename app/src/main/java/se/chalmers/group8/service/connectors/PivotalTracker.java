194ee73c6a1c883d7e81ecdc55912ae5f212351a
dollarmannen <dollarmannen@gmail.com>
2015-04-28 10:30:27 +0200

    Created a skeleton class PivotalTracker.

---
+++ app/src/main/java/se/chalmers/group8/service/connectors/PivotalTracker.java
@@ -0,0 +1,34 @@
+package se.chalmers.group8.service.connectors;
+
+/**
+ * Created by patrik on 2015-04-28.
+ */
+public class PivotalTracker {
+
+
+
+    public String create() {
+
+
+        return null;
+    }
+
+    public String read() {
+
+
+        return null;
+    }
+
+    public String update() {
+
+
+        return null;
+    }
+
+    public String delete() {
+
+
+        return null;
+    }
+
+}

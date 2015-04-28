264004c0ecd9bc352859c7172b44401f9441e5bd
dollarmannen <dollarmannen@gmail.com>
2015-04-28 12:06:32 +0200

    Added create, readAllStories, read(Story) and update to the PivotalTracker class.

--- app/src/main/java/se/chalmers/group8/service/connectors/PivotalTracker.java
+++ app/src/main/java/se/chalmers/group8/service/connectors/PivotalTracker.java
@@ -1,34 +1,93 @@
 package se.chalmers.group8.service.connectors;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import se.chalmers.group8.communication.http.Connector;
+import se.chalmers.group8.communication.http.ConnectorResult;
+import se.chalmers.group8.communication.http.RequestPropertyPair;
+
 /**
  * Created by patrik on 2015-04-28.
  */
-public class PivotalTracker {
+public class PivotalTracker implements ConnectorResult {
 
 
+    private Connector connector;
 
-    public String create() {
+    private String baseURL;
+    private String storiesURL;
+    private String projectURL;
+
+    private String token;
+    private String projectID;
+
+    private UpdateFinish updateFinish;
 
 
-        return null;
+    public PivotalTracker(String token, UpdateFinish updateFinish) {
+        this.token = token;
+        baseURL = "https://www.pivotaltracker.com/services/v5/projects";
+        projectID = "";
+        storiesURL = "";
+        projectURL = "";
+        this.updateFinish = updateFinish;
+
+        connector = new Connector(this);
     }
 
-    public String read() {
+    public void create(String name, String description) throws MalformedURLException{
+        URL url = new URL(storiesURL);
 
+        RequestPropertyPair rpp[] = new RequestPropertyPair[2];
+        rpp[0] = new RequestPropertyPair("X-TrackerToken", token);
+        rpp[1] = new RequestPropertyPair("Content-Type", "application/json");
 
-        return null;
+        String data = "{\"name\":" + "\"" +  name + "\"" + ",\"description\":" + "\"" + description + "\"" + "}";
+
+        connector.doHttpRequest(url, Connector.METHOD_POST, data, rpp);
     }
 
-    public String update() {
-
-
-        return null;
+    public void readAllStories() throws MalformedURLException{
+        readStory("");
     }
 
-    public String delete() {
 
 
-        return null;
+    public void readStory(String storyID) throws MalformedURLException {
+        String storyURL = storiesURL + storyID;
+        URL url = new URL(storyURL);
+
+        RequestPropertyPair rpp = new RequestPropertyPair("X-TrackerToken", token);
+
+        connector.doHttpRequest(url, Connector.METHOD_GET, rpp);
+
     }
 
+    public void update(String storyID, String data) throws MalformedURLException{
+        URL url = new URL(storiesURL + storyID);
+
+        RequestPropertyPair rpp[] = new RequestPropertyPair[2];
+        rpp[0] = new RequestPropertyPair("X-TrackerToken", token);
+        rpp[1] = new RequestPropertyPair("Content-Type", "application/json");
+
+        connector.doHttpRequest(url, Connector.METHOD_PUT, data, rpp);
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void setProjectID(String projectID) {
+        this.projectID = projectID;
+        projectURL = baseURL + "/" + this.projectID;
+        storiesURL = projectURL + "/stories/";
+    }
+
+
+    @Override
+    public void onConnectorResult(String result) {
+        updateFinish.onUpdateFinished(result);
+    }
 }
